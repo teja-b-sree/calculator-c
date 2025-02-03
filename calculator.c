103bc0dbd368a266bf9f1bd4af9b8ff1cf44492d
teja-b-sree <btejasree927@gmail.com>
2025-02-03 14:31:20 +0530

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    char operator;
+    double num1, num2, result;
+
+    // Taking user input
+    printf("Enter an operator (+, -, *, /): ");
+    scanf("%c", &operator);
+
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &num1, &num2);
+
+    // Performing calculation
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 != 0)
+                result = num1 / num2;
+            else {
+                printf("Error! Division by zero.\n");
+                return 1;
+            }
+            break;
+        default:
+            printf("Invalid operator!\n");
+            return 1;
+    }
+
+    // Display result
+    printf("Result: %.2lf\n", result);
+    return 0;
+}
